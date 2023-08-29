05cf4f05356ebfb3e82d5948e393c2ed312805b0
Rafael Medeiros Gomes <devmedeiros@pm.me>
2023-08-29 20:26:29 -0300

    adds type info for global variables

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+import { Chainable } from "./Chainable"
+
+declare global {
+  var Scenario: (description: string, callback: () => void) => void;
+  var Given: (description: string, callback: () => void) => void;
+  var When: (description: string, callback: () => void) => void;
+  var Then: <T>(actual: T) => Chainable<T>;
+}
+
+export { }
