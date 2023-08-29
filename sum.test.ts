7a9054ffdaff27b4fc5bfc842259eabfa4af8579
Rafael Medeiros Gomes <devmedeiros@pm.me>
2023-08-29 20:27:11 -0300

    adds test for sum function

---
+++ sum.test.ts
@@ -0,0 +1,25 @@
+import { sum } from "../src/sum"
+
+Scenario("calculating the sum of two numbers", function (this: any) {
+  Given("the numbers 1 and 2", function () {
+    const a = 1
+    const b = 2
+
+    When("calculating their sum", function () {
+      const actual = sum(a, b)
+
+      Then(actual).should.be.equal(3)
+    })
+  })
+
+  Given("the numbers 1 and 2", function () {
+  const a = 1
+  const b = 2
+
+    When("calculating their sum", function () {
+      const actual = sum(a, b)
+
+      Then(actual).should.be.equal(4)
+    })
+  })
+})
