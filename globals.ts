202a9765fcf5fd4066fda771d05eba49a0823b5e
Rafael Medeiros Gomes <devmedeiros@pm.me>
2023-08-29 20:26:18 -0300

    adds top level functions

---
+++ globals.ts
@@ -0,0 +1,13 @@
+import { Printer } from "./Printer"
+import { describe } from "./describe"
+import { it } from "./it"
+
+const printer = new Printer()
+
+global.Scenario = describe("Scenario", printer)
+
+global.Given = describe("Given", printer)
+
+global.When = describe("When", printer)
+
+global.Then = it(printer)
