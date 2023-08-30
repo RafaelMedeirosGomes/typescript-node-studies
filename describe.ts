76e5d9aa29501e4041772333dac0f5c5d5fea14e
Rafael Medeiros Gomes <devmedeiros@pm.me>
2023-08-29 21:07:59 -0300

    adds skip to describers

--- describe.ts
+++ describe.ts
@@ -1,10 +1,16 @@
 import { Printer } from "./Printer"
 
 export function describe(type: string, printer: Printer) {
-  return function (description: string, callback: () => void | Promise<void>) {
+  const describe = function (description: string, callback: () => void | Promise<void>) {
     printer.indent()
     printer.log(`${type}: ${description}`)
     callback()
     printer.unindent()
   }
+  describe.skip = function (description: string, _: () => void | Promise<void>) {
+    printer.indent()
+    printer.skip(`${type}: ${description}`)
+    printer.unindent()
+  }
+  return describe
 }
--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,18 @@
 import { Chainable } from "./Chainable"
 
 declare global {
-  var Scenario: (description: string, callback: () => void) => void;
-  var Given: (description: string, callback: () => void) => void;
-  var When: (description: string, callback: () => void) => void;
+  var Scenario: {
+    (description: string, callback: () => void): void
+    skip(description: string, callback: () => void): void
+  };
+  var Given: {
+    (description: string, callback: () => void): void
+    skip(description: string, callback: () => void): void
+  };
+  var When: {
+    (description: string, callback: () => void): void
+    skip(description: string, callback: () => void): void
+  };
   var Then: <T>(actual: T) => Chainable<T>;
 }
 
