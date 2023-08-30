bd24d5a3c181d74cdc44f76f74d2bc65250892e2
Rafael Medeiros Gomes <devmedeiros@pm.me>
2023-08-29 21:08:18 -0300

    adds printer for skipped tests

--- Printer.ts
+++ Printer.ts
@@ -3,9 +3,11 @@ export class Printer {
 
   private ANSI_red_text = "\x1b[31m";
   private ANSI_green_text = "\x1b[32m";
+  private ANSI_yellow_text = "\x1b[33m";
   private ANSI_reset_text = "\x1b[0m";
   private FAIL_ICON = " ✘ ";
   private PASS_ICON = " ✔ ";
+  private SKIP_ICON = " ↩ ";
 
   fail(message: string) {
     console.log([
@@ -29,6 +31,16 @@ export class Printer {
 
   log(message: string) {
     console.log(`${' '.repeat(this.identation)}${message}`)
+  }
+
+  skip(message: string) {
+    console.log([
+      this.prepend().slice(0, -this.SKIP_ICON.length),
+      this.ANSI_yellow_text,
+      this.SKIP_ICON,
+      message,
+      this.ANSI_reset_text
+    ].join(""));
   }
 
   prepend() {
