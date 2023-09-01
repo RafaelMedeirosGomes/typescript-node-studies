8b9f28582ddadc2a0eaac6d1d287770fe6107bc9
Rafael Medeiros Gomes <devmedeiros@pm.me>
2023-09-01 17:33:11 -0300

    adds file loading logic

--- main.ts
+++ main.ts
@@ -1,5 +1,30 @@
+import FS from "node:fs/promises"
+import PATH from "node:path"
+
 import "./globals"
 
-export function main() {
-    import("../tests/sum.test")
+export async function main() {
+    const testDir = PATH.join(__dirname, "../tests")
+    
+    const filePaths = await getAllTestFiles(testDir)
+
+    const results = await Promise.allSettled(filePaths.map((path) => import(path)))
+    
+    results.forEach(result => result.status === "rejected" && console.error(result.reason))
+}
+
+async function getAllTestFiles(path: string) {
+    const filePaths: string[] = []
+
+    const entities = await FS.readdir(path, { withFileTypes: true, recursive: true })
+
+    for (let i = 0; i < entities.length; i++) {
+        const dirent = entities[i];
+
+        if (dirent.isFile() && dirent.name.endsWith(".test.js")) {
+            filePaths.push(dirent.path + "/" + dirent.name.slice(0, -3))
+        }   
+    }
+
+    return filePaths
 }
